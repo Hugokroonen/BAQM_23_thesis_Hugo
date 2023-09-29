25626f988e6cf5d99dd3c3de89dd46f917c0285a
Hugo Kroonen <95935259+Hugokroonen@users.noreply.github.com>
2023-09-29 11:54:34 +0200

    Update descriptive_statistics.py

--- thesis/descriptive/descriptive_statistics.py
+++ thesis/descriptive/descriptive_statistics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the CSV file into a pandas DataFrame
-x = pd.read_csv('data/x_thesis.csv')
+x = pd.read_csv('data/x.csv')
 
 # Calculate the average of the first column
 average_first_column = x.iloc[:, 0].mean()
@@ -14,16 +14,16 @@ print(f"Average Weekend: {average_second_column*100}")
 print(f"Average After 5PM: {average_third_column*100}")
 
 # Read the CSV file into a pandas DataFrame
-w = pd.read_csv('data/w_thesis.csv')
+h = pd.read_csv('data/h.csv')
 
 # Calculate the average of the first column
-average_city_column = w.iloc[:, 6].mean()
-unknown_age = w.iloc[:, 0].mean()
-age_0_25 = w.iloc[:, 1].mean()
-age_25_35 = w.iloc[:, 2].mean()
-age_35_45 = w.iloc[:, 3].mean()
-age_55_65 = w.iloc[:, 4].mean()
-age_65 = w.iloc[:, 5].mean()
+average_city_column = h.iloc[:, 6].mean()
+unknown_age = h.iloc[:, 0].mean()
+age_0_25 = h.iloc[:, 1].mean()
+age_25_35 = h.iloc[:, 2].mean()
+age_35_45 = h.iloc[:, 3].mean()
+age_55_65 = h.iloc[:, 4].mean()
+age_65 = h.iloc[:, 5].mean()
 
 print(f"Average city: {average_city_column*100}")
 print(f"Average unknown: {unknown_age*100}")
@@ -33,9 +33,9 @@ print(f"Average 35-45: {age_35_45*100}")
 print(f"Average 55-65: {age_55_65*100}")
 print(f"Average 65+: {age_65*100}")
 
-w = pd.read_csv('data/w_thesis.csv', header=None)  # No header information provided
-y = pd.read_csv('data/y_thesis.csv', header=None)
-final_sample = pd.read_csv('data/finalsample_thesis.csv')
+h = pd.read_csv('data/h.csv', header=None)  # No header information provided
+y = pd.read_csv('data/y.csv', header=None)
+final_sample = pd.read_csv('data/products.csv')
 
 # Find the top 10 most occurring values in the third column of y
 top_product_ids = y.iloc[:, 2].value_counts().head(10).index.tolist()
@@ -47,7 +47,7 @@ product_id_lowest_level = filtered_final_sample[['product_id', 'lowest_level']].
 # Count unique baskets for each top product
 basket_counts = {}
 for product_id in top_product_ids:
-    baskets_with_product = y[y.iloc[:, 2] == product_id].iloc[:, 1].nunique()  # Assuming second column contains basket IDs
+    baskets_ith_product = y[y.iloc[:, 2] == product_id].iloc[:, 1].nunique()  # Assuming second column contains basket IDs
     basket_counts[product_id] = baskets_with_product
 
 # Calculate percentages
